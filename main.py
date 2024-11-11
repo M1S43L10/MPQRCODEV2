b64707150ff770f0f21e22cf90fae2458c297eff
M1S43L10 <misaelr893@gmail.com>
2024-11-11 16:10:53 -0300

    3.0.1

---
+++ main.py
@@ -0,0 +1,12 @@
+from Conf.GUIENLACECREARORDENV2 import EnlaceCrearOrden
+
+__version__ = "3.0.1"
+
+"""
+2.0.0 Nueva version, mejor funcionamiento, se agregaroon la funcionalidad de threading, se usa tkbootstrap. Solo
+se puede crear un orden de pago.
+
+3.0.0 Se agrega la funcion de reembolso ademas del pago."""
+
+if __name__ == "__main__":
+    config = EnlaceCrearOrden(__version__)
